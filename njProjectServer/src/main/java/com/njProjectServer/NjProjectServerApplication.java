85215c1e6adb0f2f702b65595dcbd0aa213fb7d4
nikolakojic997 <volimdahodam@gmail.com>
2020-08-15 14:49:35 +0200

    Initial commit

---
+++ njProjectServer/src/main/java/com/njProjectServer/NjProjectServerApplication.java
@@ -0,0 +1,13 @@
+package com.njProjectServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjProjectServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjProjectServerApplication.class, args);
+	}
+
+}
---
+++ njProjectServer/src/test/java/com/njProjectServer/NjProjectServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njProjectServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjProjectServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
